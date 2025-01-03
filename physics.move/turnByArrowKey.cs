1b059436bc7180eb23d86d9a8f1ce6b8c7938bc2
tech26z <2446821304@qq.com>
2025-01-03 17:27:56 +0800

    Update turnByArrowKey.cs

--- physics.move/turnByArrowKey.cs
+++ physics.move/turnByArrowKey.cs
@@ -7,27 +7,27 @@ using UnityEngine;
 
 namespace tech26z.library.hphics.move{
 
-public class TurnByArrowKey : MonoBehaviour {
+  public class TurnByArrowKey : MonoBehaviour {
     float degree;
-	// Use this for initialization
-	void Start () {
+      // Use this for initialization
+      void Start () {
         transform.Translate(new Vector3(1, 0, 0));
         //transform.Rotate(new Vector3(0, 45, 0));
-	}
+      }
 	
-	// Update is called once per frame
-	void Update () {
+      // Update is called once per frame
+      void Update () {
         //transform.Translate(new Vector3(1, 0, 0));
         if(Input.GetKey(KeyCode.LeftArrow)){
-            transform.Rotate(new Vector3(0, 10, 0)*Time.deltaTime);
-            degree += Time.deltaTime*10;
-            Debug.Log((int)degree);
+          transform.Rotate(new Vector3(0, 10, 0)*Time.deltaTime);
+          degree += Time.deltaTime*10;
+          Debug.Log((int)degree);
         }
         else if(Input.GetKey(KeyCode.RightArrow)){
             transform.Rotate(new Vector3(0, -10, 0) * Time.deltaTime);
             degree -= Time.deltaTime * 10;
             Debug.Log((int)degree);
         }
-	}
-}
+      }
+  }
 }
